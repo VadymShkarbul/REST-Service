1284f08702eb89e39d74fdfd454b3de390b46dbd
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2023-01-16 00:49:22 +0200

    add check_file function and fix update endpoint

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
+import hashlib
 import os
 
 import boto3
-from flask import request, render_template, jsonify
+from botocore import exceptions
+from flask import request, render_template, jsonify, Response
 from werkzeug.utils import secure_filename
 
 from app import app
@@ -12,89 +14,90 @@ BUCKET = app.config["S3_BUCKET_NAME"]
 EXTRA_ARGS = {"ContentType": "application/octet-stream"}
 
 
-def check_file_size(key, local_path):
+def check_file(key: str, local_path: str) -> bool:
+    """
+    The function checks the size and content of the local file and
+    the file in the cloud and returns a boolean value
+    """
     s3_file_size = app.s3.head_object(Bucket=BUCKET, Key=key)["ContentLength"]
     local_file_size = os.path.getsize(local_path)
-    if s3_file_size != local_file_size:
-        return False
-    else:
+
+    local_hash = hashlib.sha256()
+    with open(local_path, "rb") as f:
+        local_hash.update(f.read())
+    local_file_hash = local_hash.hexdigest()
+    s3_hash = hashlib.sha256()
+    s3_object = app.s3.get_object(Bucket=BUCKET, Key=key)
+    s3_hash.update(s3_object["Body"].read())
+    s3_object_hash = s3_hash.hexdigest()
+
+    if local_file_hash == s3_object_hash and s3_file_size == local_file_size:
         return True
+    return False
 
 
 @app.route("/")
-def list_files():
+def list_files() -> str:
     files = app.s3.list_objects(Bucket=BUCKET).get("Contents", [])
     return render_template("index.html", files=files)
 
 
-@app.route('/add')
-def upload_file():
-    return render_template('upload_file.html')
+@app.route("/add")
+def upload_file() -> str:
+    return render_template("upload_file.html")
 
 
 @app.route("/upload", methods=["POST", "PUT"])
-def upload():
+def upload() -> Response:
+    """
+    If I understand the boto3 documentation correctly, under the hood
+    upload_file() and upload_fileobj() methods work in such a way
+    that if the file exists - it is updated (PUT), if not - it is uploaded(POST).
+    In my opinion, using S3Transfer is safer from the point of view of data
+    integrity, will better work with larger files(IMHO) and the condition is met:
+    the data is available via GET immediately after the PUT is completed
+    """
     file = request.files["file"]
     if request.files["file"].filename == "":
         return jsonify({"error": "No file selected"})
     else:
         file_name = secure_filename(file.filename)
-        response = app.s3.list_objects_v2(Bucket=BUCKET, Prefix=file_name)
-        if 'Contents' in response:
-            for obj in response['Contents']:
-                if obj['Key'] == file_name:
-                    # File exists, make PUT request
-                    app.s3.put_object(Bucket=BUCKET, Key=file_name, Body=file.stream)
-                    return jsonify({"message": f"File {file_name} updated successfully"})
-                    """
-                    If I understand the boto3 documentation correctly, under the hood 
-                    the upload_file() and upload_fileobj() methods work in such a way
-                    that if the file exists - it is updated, if not - it is uploaded. 
-                    I left this code because in the condition of the test task 
-                    I had to implement the put operation and it was interesting 
-                    to work with this method as well ;)
-                    """
+        local_path = f"{UPLOAD_DIR}/{file_name}"
+        file.save(local_path)
+        transfer = boto3.s3.transfer.S3Transfer(app.s3)
+
+        try:
+            app.s3.head_object(Bucket=BUCKET, Key=file_name)
+        except exceptions.ClientError:
+            transfer.upload_file(local_path, BUCKET, file_name, extra_args=EXTRA_ARGS)
+            response = jsonify({"message": f"File {file_name} uploaded successfully"})
         else:
-            # File does not exist, make POST request
-            local_path = f"{UPLOAD_DIR}/{file_name}"
-            file.save(local_path)
-            transfer = boto3.s3.transfer.S3Transfer(app.s3)
-            transfer.upload_file(local_path, BUCKET, file_name)
-            if check_file_size(file_name, local_path):
-                os.remove(local_path)
-                return jsonify({"message": f"File {file_name} uploaded successfully"})
+            transfer.upload_file(local_path, BUCKET, file_name, extra_args=EXTRA_ARGS)
+            response = jsonify({"message": f"File {file_name} updated successfully"})
+        finally:
             os.remove(local_path)
-            return jsonify({"error": f"File {file_name} uploaded not completely"})
-            """
-            # I chose this method because, in my opinion, using S3Transfer is safer
-            # from the point of view of data integrity and will allow working with larger files
-            # I haven't found a more elegant way to do this...
-            Flask-Uploads + boto3?
-            """
-            # app.s3.upload_fileobj(file.stream, BUCKET, file_name)
+
+        return response
 
 
 @app.route("/download", methods=["GET"])
-def download():
+def download() -> Response:
     file_name = request.args.get("file_name")
     local_path = f"{DOWNLOAD_DIR}/{file_name}"
-    if os.path.exists(local_path):
+    if os.path.exists(local_path) and check_file(file_name, local_path):
         return jsonify({"message": f"File {file_name} already exists locally"})
     else:
         transfer = boto3.s3.transfer.S3Transfer(app.s3)
         transfer.download_file(BUCKET, file_name, local_path)
-        if check_file_size(file_name, local_path):
-            return jsonify({"message": f"File {file_name} downloaded successfully"})
-        else:
-            return jsonify({"error": f"File {file_name} downloaded not completely"})
+        return jsonify({"message": f"File {file_name} downloaded successfully"})
 
 
 @app.route("/delete", methods=["GET", "DELETE"])
-def delete():
+def delete() -> Response:
     file_name = request.args.get("file_name")
     app.s3.delete_object(Bucket=BUCKET, Key=file_name)
     return jsonify({"message": f"File {file_name} deleted successfully"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
